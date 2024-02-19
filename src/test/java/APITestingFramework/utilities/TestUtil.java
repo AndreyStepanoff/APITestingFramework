407062299e3a31b624169c59e10a8402dcb180b6
AndreyStepanoff <andrey.stepanov.tr@gmail.com>
2024-02-19 13:52:56 +0100

    Adding new testcases

--- src/test/java/APITestingFramework/utilities/TestUtil.java
+++ src/test/java/APITestingFramework/utilities/TestUtil.java
@@ -1,7 +1,14 @@
 package APITestingFramework.utilities;
 
 import APITestingFramework.listeners.ExtentListeners;
+import com.google.gson.internal.bind.util.ISO8601Utils;
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class TestUtil {
 
@@ -15,5 +22,19 @@ public class TestUtil {
         JSONObject jsonObject = new JSONObject(json);
         ExtentListeners.testReport.get().info("Validating the key value" + key);
         return jsonObject.get(key).toString();
+    }
+
+    public static int countJsonKeys(String json, String key){
+        JSONObject jsonObject = new JSONObject(json);
+        JSONArray dataArray = jsonObject.getJSONArray("data");
+        Set<String> uniqueIds = new HashSet<>();
+        for (int i=0;i<dataArray.length();i++){
+            JSONObject dataObject = dataArray.getJSONObject(i);
+            String id = dataObject.getString("id");
+            uniqueIds.add(id);
+        }
+        int uniqIDcount = uniqueIds.size();
+        return uniqIDcount;
+
     }
 }
